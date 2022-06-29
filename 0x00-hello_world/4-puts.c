f4a4eddb96dda27ce841b754347807a4049e2da6
dreamboxtech <bbbolaleye@gmail.com>
2022-06-28 22:09:38 -0700

    C String!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-/*
- *The main function
- *Description goes here...
- */
-int main(void)
-{
-	/* This sets a string and puts it out 
-	 *Returns int zer0 as declared
-	 */
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
-}
 
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+			puts("\"Programming is like building a multilingual puzzle");
+
+						return (0);
+
+}
