09b49feecb1aaba8417241d73cd8b066ac72ff9a
dreamboxtech <bbbolaleye@gmail.com>
2022-06-28 23:32:47 -0700

    Without puts and printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - does the main function
+ *
+ * Return: this time returns a 1
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
