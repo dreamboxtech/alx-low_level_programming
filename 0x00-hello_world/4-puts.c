eb23944e2346d1a100bdd8d9d5d287f3c60e6d98
dreamboxtech <bbbolaleye@gmail.com>
2022-06-28 21:53:10 -0700

    C String!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-	//This sets a string and puts it out
+	/* This sets a string and puts it out */
 	char st[] =  "\"Programming is like building a multilingual puzzle";
 	puts(st);
 	return (0);
