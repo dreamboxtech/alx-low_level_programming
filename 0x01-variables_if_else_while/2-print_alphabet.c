229899b1e8ee47e76196f4ebed05d08091f070ef
dreamboxtech <bbbolaleye@gmail.com>
2022-07-01 12:52:10 -0700

    All Aplhs

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	for(int x = 'A'; x <= 'Z'; x++)
+	for (int x = 'A'; x <= 'Z'; x++)
 	{
-		    putchar(tolower(x));
-		    putchar("\n")
+		putchar(tolower(x));
+		putchar("\n")
 	}
 	return (0);
 }
