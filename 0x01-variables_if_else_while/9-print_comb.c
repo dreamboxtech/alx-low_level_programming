4b4f85b33a5d4235fedb99a9f1b0a10f77e25cbf
dreamboxtech <bbbolaleye@gmail.com>
2022-07-01 15:56:45 -0700

    Combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -does main thing
+ *
+ * Return: returns the main functions
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; ++x)
+	{
+		putchar(x);
+		if (x != 9)
+		{
+			putchar(", ");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
