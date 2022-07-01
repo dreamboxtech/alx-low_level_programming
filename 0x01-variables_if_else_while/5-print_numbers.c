ab757300ce3e769b717da0470e4d0dbdaedb8fa4
dreamboxtech <bbbolaleye@gmail.com>
2022-07-01 14:48:35 -0700

    log of single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main -does main thing
+ *
+ * Return: returns the main functions
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", log10(x));
+	}
+	putchar('\n');
+	return (0);
+}
