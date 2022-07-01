81f17f69732e169c34494d7bf17a0494d61b77ce
dreamboxtech <bbbolaleye@gmail.com>
2022-07-01 12:41:31 -0700

    All Aplhs

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -does main thing
+ *
+ * Return: returns the main functions
+ */
+
+int main(void)
+{
+	for(int x = 'A'; x <= 'Z'; x++)
+	{
+		    putchar(tolower(x));
+		    putchar("\n")
+	}
+	return (0);
+}
