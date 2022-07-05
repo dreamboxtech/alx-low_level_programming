df8689c25dd10d59f44758ebada09e659f471ec9
dreamboxtech <bbbolaleye@gmail.com>
2022-07-05 08:31:45 -0700

    Last Number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_last_digit - doing things
+ *
+ * @l: Input parametre
+ * Return: int
+ */
+
+int print_last_digit(int l)
+{
+	int a;
+
+	if (l < 0)
+	{
+		a = -1 * (l % 10);
+	}
+	else
+	{
+		a = l % 10;
+	}
+	_putchar((a % 10) + '0');
+	return (n % 10);
+}
