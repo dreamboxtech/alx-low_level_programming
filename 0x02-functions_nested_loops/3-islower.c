c06f6ffa1d0fe6571dba24ccb013fc1b5c85d7f1
dreamboxtech <bbbolaleye@gmail.com>
2022-07-05 07:30:04 -0700

    find islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - doing things
+ *
+ * Return: int
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
