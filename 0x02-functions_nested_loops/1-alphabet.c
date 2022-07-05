27a5d25b9191a6f1846b27599d3bd635cf9d6a66
dreamboxtech <bbbolaleye@gmail.com>
2022-07-05 06:46:32 -0700

    All Alphabelts lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet - prints all alphs
+ *
+ * Return: really, nothing
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l < 'z'; l++)
+	{
+		_putchar(l)
+	}
+	_putchar('\n')
+}
