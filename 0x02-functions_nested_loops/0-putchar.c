990cfe06b604085a88d05b00aee074f87e62fc91
dreamboxtech <bbbolaleye@gmail.com>
2022-07-05 05:19:41 -0700

    Use Putchar only

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - does som main tasks
+ * Return: Returns main's
+ */
+int main(void)
+
+{  
+	char* st = "_putchar\n";
+        int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(st[i])
+	}
+	return (0);
+{
