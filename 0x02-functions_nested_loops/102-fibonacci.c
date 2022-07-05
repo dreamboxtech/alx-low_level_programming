0497327bd531ed281de0b919239e050ab2c148d2
dreamboxtech <bbbolaleye@gmail.com>
2022-07-05 14:29:36 -0700

    First 50 Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,47 +7,25 @@
  */
 
 int main(void)
-
 {
-
 	unsigned long c, i, j, k;
-
-
-
 	i = 0;
-
 	j = 1;
 
 	for (c = 0; c < 50; c++)
-
 	{
-
 		k = i + j;
-
 		i = j;
-
 		j = k;
-
 		printf("%lu", k);
-
 		if (c == 49)
-
 		{
-
 			printf("\n");
-
 		}
-
 		else
-
 		{
-
 			printf(", ");
-
 		}
-
 	}
-
 	return (0);
-
 }
