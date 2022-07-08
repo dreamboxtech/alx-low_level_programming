093e788f31b1fb8735fabf2d2d948d52eb11296b
dreamboxtech <bbbolaleye@gmail.com>
2022-07-08 08:12:24 -0700

    Multiply 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Does the function of checking
+ * @a: variable to be tested
+ * @b: Second var to be tested
+ * Return: 1 or 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
