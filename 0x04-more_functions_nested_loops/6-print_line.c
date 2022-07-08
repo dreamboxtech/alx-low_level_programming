a70e45b805553e955d0c564c46c0c56b12370bd3
dreamboxtech <bbbolaleye@gmail.com>
2022-07-08 10:17:39 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Does the function of checking
+ * @n: Parameter to be supplied
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (n = 1; n <= n; n++)
+		{
+			_putchar('-');
+		}
+		_putchar('\n');
+	}
+}
