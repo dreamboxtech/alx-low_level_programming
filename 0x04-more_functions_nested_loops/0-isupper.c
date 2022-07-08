1b60668b544cbe3280ff13c17ae4bf89efe852ec
dreamboxtech <bbbolaleye@gmail.com>
2022-07-08 07:37:09 -0700

    Upper or Lower

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cytype.h>
+#include "main.h"
+
+/**
+ * _isupper - Does the function of checking
+ * @c: variable
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1)
+	}
+	else
+	{
+		return (0)
+	}
+}
