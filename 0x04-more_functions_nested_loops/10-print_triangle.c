739e033c22d4c991f7aabde4f32dece11f10377a
dreamboxtech <bbbolaleye@gmail.com>
2022-07-08 13:31:57 -0700

    Add a traingle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Does the function of checking
+ * @size: Parameter to be supplied
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - i); j++)
+		{
+			_putchar(' ');
+		}
+		for (j--; j < size; j++)
+		{
+			_putchar(35);
+		}
+		if (i < (size - 1))
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
