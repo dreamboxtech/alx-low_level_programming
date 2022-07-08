93b5951bedc793feed15aa1f9875cd2dad543b65
dreamboxtech <bbbolaleye@gmail.com>
2022-07-08 14:24:02 -0700

    Print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ * @n: Input number
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	int res, temp, expo;
+
+	if (n < 0)
+	{
+		res = n * -1;
+		_putchar(45);
+	}
+	else
+	{
+		res = n;
+	}
+	temp = res;
+	expo = 1;
+	while (temp >= 10)
+	{
+		temp /= 10;
+		expo *= 10;
+	}
+	for (; expo >= 1; expo /= 10)
+	{
+		_putchar(((res / expo) % 10) + 48);
+	}
+}
