d54cd75bcd3f5698a9e236621698eba82054f489
dreamboxtech <bbbolaleye@gmail.com>
2022-07-08 08:27:31 -0700

    Print 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Does the function of checking
+ * Return: void
+ */
+
+void return_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
