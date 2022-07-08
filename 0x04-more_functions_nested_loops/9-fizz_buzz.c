1a55e63d23a6ba2908d57f8c5cec91294114846f
dreamboxtech <bbbolaleye@gmail.com>
2022-07-08 13:56:04 -0700

    FizzBuzz Test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,9 @@ int  main(void)
 	
 	for (i = 1; i <= 100; i++)
 	{
-		if (i == 1)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("%d", i);
+			printf("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
@@ -22,10 +22,6 @@ int  main(void)
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
-		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz ");
 		}
 		else
 		{
