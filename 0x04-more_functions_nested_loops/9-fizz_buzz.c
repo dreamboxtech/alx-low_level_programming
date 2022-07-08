1b5e3583c3c381627be3acc2f939c5f23b9ea645
dreamboxtech <bbbolaleye@gmail.com>
2022-07-08 12:08:34 -0700

    Print FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -10,7 +9,7 @@ int  main(void)
 {
 	int i;
 	
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0)
 		{
@@ -29,5 +28,6 @@ int  main(void)
 			printf("%d ", i);
 		}	
 	}
+	prinf("Buzz\n");
 	return (0);
 }
