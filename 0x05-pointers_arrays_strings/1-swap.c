919167fbe2228bc301c9e2228cdfa8f82e39c0a4
dreamboxtech <bbbolaleye@gmail.com>
2022-07-12 06:03:38 -0700

    swaped pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * void swap_int - does main task
+ * @a: is a pointer
+ * @b: second integer
+ * Return: returns as 0
+ */
+
+void void swap_int(int *a  int *b)
+{
+	int p = *a;
+	
+	*a = *b;
+
+	*b = p;
+}
