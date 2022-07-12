a25c76d8a7f4332acebfa4b656b28278c144efcc
dreamboxtech <bbbolaleye@gmail.com>
2022-07-12 09:14:08 -0700

    Reversed Words!

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - does main task
+ * @s: is value
+ * Return: returns as 0
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int c = 0;	
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		c++;
+	}
+	while (c)
+        {
+           _putchar(s[--c]); 
+        }
+	_putchar('\n');
+}
