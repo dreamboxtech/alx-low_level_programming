fa8191468fb593c180b73f7610d364a7e26bf331
dreamboxtech <bbbolaleye@gmail.com>
2022-07-12 08:45:53 -0700

    Use _putchar to write out!

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - does main task
+ * @str: is a pointer
+ * Return: returns as 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
