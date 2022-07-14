83eb3b1f2040e11052c16afb96a04c86ae79d1f0
dreamboxtech <bbbolaleye@gmail.com>
2022-07-14 12:26:57 -0700

    Rot13 Encoding implemetation

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - does main task
+ * @s: parameter one
+ * Return: Mostly a 0
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char *a = "ABCDEFGHIJKLMabcdefghijklm";
+	char *b = "NOPQRSTUVWXYZnopqrstuvwxyz";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+
+	return (s);
+}
