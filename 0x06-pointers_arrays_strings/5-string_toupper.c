b2009521e373f99d0e2084448e883de51edd1599
dreamboxtech <bbbolaleye@gmail.com>
2022-07-14 10:03:46 -0700

    Change lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - does main task
+ * @s: parameter one
+ * Return: Mostly a 0
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+
+	return (s);
+}
