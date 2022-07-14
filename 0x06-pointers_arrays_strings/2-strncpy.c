c87ff968463eed5b4a33ab4766a0dd03c14092f0
dreamboxtech <bbbolaleye@gmail.com>
2022-07-14 08:32:27 -0700

    String copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,23 +10,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
+	int i;
 
-	while (*src)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		srclen++;
-		src++;
+		dest[i] = src[i];
 	}
-	srclen++;
-	if (n > srclen)
+	while (i < n)
 	{
-		n = srclen;
+		dest[i] = '\0';
+		i++;
 	}
-	src = start;
-	for (; i < n; i++)
-	{
-		*dest++ = *src++;
-	}
-	return (temp);
+	return (dest);
 }
