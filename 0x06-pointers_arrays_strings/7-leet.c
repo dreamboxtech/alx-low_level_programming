0884d003cd42239188b461d8591bcc212a24c647
dreamboxtech <bbbolaleye@gmail.com>
2022-07-14 11:08:41 -0700

    Convert to 137

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,19 +9,20 @@
 char *leet(char *s)
 {
 	int i, j;
-	
-	char *a = "AaEeOoTtlL";
+
+	char *a = "aAeEoOtTlL";
 	char *b = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (a[j] == s[i])
+			if (s[i] == a[j])
 			{
 				s[i] = b[j];
 			}
 		}
 	}
+
 	return (s);
 }
