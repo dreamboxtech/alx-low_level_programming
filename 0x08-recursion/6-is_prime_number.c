21f5c1846db00d2a2d66c8512b95fae37f82c38b
dreamboxtech <bbbolaleye@gmail.com>
2022-07-20 06:35:40 -0700

    If prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -21,15 +21,10 @@ int is_prime_number(int n)
 }
 
 /**
-
  * is_divisible - check if num is divisible
-
  * @num: the number to be checked
-
  * @div: the result of division
-
  * Return: 1 if num is divisible or 0 if numis not divisible
-
  */
 
 int is_divisible(int num, int div)
