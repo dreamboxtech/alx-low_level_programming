1ecb6ba5a03a132f3b2b186af9c889a0b1f1535d
dreamboxtech <bbbolaleye@gmail.com>
2022-07-20 05:38:04 -0700

    This is reverse string with recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * void _print_rev_recursion - Our very function
+ * @s: The parameter to be supplied
+ * Return: ) for the most part
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
