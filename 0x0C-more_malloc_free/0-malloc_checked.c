d0075ad46e0bfb8d5cace4d13a4749248c2c74c9
dreamboxtech <bbbolaleye@gmail.com>
2022-07-28 16:55:16 -0700

    assign memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 /**
- * *malloc_checked - allocates memory using malloc and exit if failed
+ * malloc_checked - allocates memory using malloc and exit if failed
  * @b: int
  * Return: pointer to the array initialized or NULL
  */
