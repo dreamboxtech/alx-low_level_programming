3534d98d1d1ee91d4ed4ebe4e165d8b6bd1dc57a
dreamboxtech <bbbolaleye@gmail.com>
2022-08-01 08:19:42 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,51 +1,50 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * new_dog - creates a new dog structure
- * @name: struct parameter name
- * @age: struct parameter age
- * @owner: struct parameter owner
- * Return: returns pointer to buffer of datatype dog
+ * new_dog - new dog
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's dog
+ * Return: newdog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int n, o, i;
-	dog_t *d;
 
-	n = o = 0;
-	while (name[n++])
-		;
-	while (owner[o++])
-		;
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
-	{
-		return (NULL);
-	}
-	d->name = malloc(n * sizeof(d->name));
-	if (d == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < n; i++)
-	{
-		d->name[i] = name[i];
-	}
-	d->age = age;
-	d->owner = malloc(o * sizeof(d->owner));
-	if (d == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < o; i++)
-	{
-		d->owner[i] = owner[i];
-	}
+	int i = 0, j = 0, k;
+	dog_t *doge;
 
-	return (d);
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	doge = malloc(sizeof(dog_t));
+	if (doge == NULL)
+	{
+		free(doge);
+		return (NULL);
+	}
+	doge->name = malloc(i * sizeof(doge->name));
+	if (doge->name == NULL)
+	{
+		free(doge->name);
+		free(doge);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		doge->name[k] = name[k];
+	doge->age = age;
+	doge->owner = malloc(j * sizeof(doge->owner));
+	if (doge->owner == NULL)
+	{
+		free(doge->owner);
+		free(doge->name);
+		free(doge);
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+		doge->owner[k] = owner[k];
+	return (doge);
 }
-
-
-
